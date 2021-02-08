9de2c58f732da0796fbb878525d61819588d1b90
Duha Jarrar <54679733+duhajarrar@users.noreply.github.com>
2021-02-08 17:59:51 +0200

    Update done_last.ino

--- done_last/done_last.ino
+++ done_last/done_last.ino
@@ -1,3 +1,10 @@
+// Parameters
+const int drive_distance = 1000;   // cm
+const int motor_power = 150;      // 0-255
+const int motor_offset = 5;       // Diff. when driving straight
+const int wheel_d = 66;           // Wheel diameter (mm)
+const float wheel_c = PI * wheel_d; // Wheel circumference (mm)
+const int counts_per_rev = 960;   // (4 pairs N-S) * (48:1 gearbox) * (2 falling/rising edges) = 384
 
 // for 3 Ultra Sensor
 int trigPinL =A2;
@@ -27,11 +34,13 @@ int wall_threshold =12;
 int front_threshold = 12;
 
 
-const byte MOTOR1 = A1;  // Motor 1 Interrupt Pin - INT 0
-const byte MOTOR2 = A0;  // Motor 2 Interrupt Pin - INT 1
+// Globals
+volatile unsigned long enc_l = 0;
+volatile unsigned long enc_r = 0;
 
-
- 
+ // Pins
+const int enc_l_pin = A1;          // Motor A
+const int enc_r_pin = A0;          // Motor B
 
 int in1 = 6; //R
 int in2 = 7; 
@@ -65,8 +74,13 @@ void setup() {
   
 //setDirection(FORWARD);
  
- 
+  pinMode(enc_l_pin, INPUT_PULLUP);
+  pinMode(enc_r_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(enc_l_pin), countLeft, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(enc_r_pin), countRight, CHANGE);
   
+  Serial.println(enc_l);
+  Serial.println(enc_r);  
 
 }
 
@@ -238,4 +252,183 @@ void setDirection(int dir) {
   digitalWrite(in4, HIGH);
 
   }  
+}
+//****************************************************************************************************************
+/*void driveStraight(float dist, int power) {
+  unsigned long num_ticks_l;
+  unsigned long num_ticks_r;
+
+  // Set initial motor power
+  int power_l = motor_power;
+  int power_r = motor_power;
+
+  // Used to determine which way to turn to adjust
+  unsigned long diff_l;
+  unsigned long diff_r;
+
+  // Reset encoder counts
+  enc_l = 0;
+  enc_r = 0;
+
+  // Remember previous encoder counts
+  unsigned long enc_l_prev = enc_l;
+  unsigned long enc_r_prev = enc_r;
+
+  // Calculate target number of ticks
+  float num_rev = (dist * 10) / wheel_c;  // Convert to mm
+  unsigned long target_count = num_rev * counts_per_rev;
+  
+  // Debug
+  //Serial.print("Driving for ");
+  //Serial.print(dist);
+ // Serial.print(" cm (");
+  //Serial.print(target_count);
+//  Serial.print(" ticks) at ");
+//  Serial.print(power);
+//  Serial.println(" motor power");
+
+  // Drive until one of the encoders reaches desired count
+  while ( (enc_l < target_count) && (enc_r < target_count) ) {
+    ReadSensors();
+    walls(); 
+    Serial.print("wallLeft ");
+    Serial.println(wallLeft);
+    Serial.print("wallRight ");
+    
+    Serial.println(wallRight);
+    Serial.print("wallFront ");
+    Serial.println(wallFront);
+   
+    if(!wallFront){
+    // Sample number of encoder ticks
+    num_ticks_l = enc_l;
+    num_ticks_r = enc_r;
+
+    // Print out current number of ticks
+//    Serial.print(num_ticks_l);
+//    Serial.print("\t");
+//    Serial.println(num_ticks_r);
+
+    // Drive
+    drive(power_l, power_r);
+
+    // Number of ticks counted since last time
+    diff_l = num_ticks_l - enc_l_prev;
+    diff_r = num_ticks_r - enc_r_prev;
+
+    // Store current tick counter for next time
+    enc_l_prev = num_ticks_l;
+    enc_r_prev = num_ticks_r;
+
+    // If left is faster, slow it down and speed up right
+    if ( diff_l > diff_r ) {
+      power_l -= motor_offset;
+      power_r += motor_offset;
+    }
+
+    // If right is faster, slow it down and speed up left
+    if ( diff_l < diff_r ) {
+      power_l += motor_offset;
+      power_r -= motor_offset;
+    }
+
+    // Brief pause to let motors respond
+   // delay(20);
+  }else if(!wallLeft){
+      
+      digitalWrite(ain1_pin, LOW); //left 90 degree
+    digitalWrite(ain2_pin, HIGH);
+    digitalWrite(bin1_pin, LOW);
+    digitalWrite(bin2_pin, HIGH);
+    analogWrite(pwma_pin, 100);
+  analogWrite(pwmb_pin, 100);
+    delay(100);
+      int l=enc_l;
+      while(((enc_l-l)) <  (int)(((32*PI/4)/wheel_c)*counts_per_rev)){
+    digitalWrite(ain1_pin, HIGH);
+    digitalWrite(ain2_pin, LOW);
+    digitalWrite(bin1_pin, LOW); //left 90 degree
+    digitalWrite(bin2_pin, LOW);
+      
+      }
+      l=0;
+ //   analogWrite(pwma_pin, 255);
+//analogWrite(pwmb_pin, 50);
+  Serial.println("left 90 degree ");
+  //  delay(200);
+  //  brake();
+    //delay(10000);
+  }
+  else if (!wallRight){
+      digitalWrite(ain1_pin, LOW); //back 
+    digitalWrite(ain2_pin, HIGH);
+    digitalWrite(bin1_pin, LOW);
+    digitalWrite(bin2_pin, HIGH);
+    analogWrite(pwma_pin, 100);
+  analogWrite(pwmb_pin, 100);
+    delay(100);
+    
+    int r=enc_r;
+      while(((enc_r-r)) <  (int)(1+((32*PI/4)/wheel_c)*counts_per_rev)){
+    digitalWrite(ain1_pin, LOW); //right 90 degree
+    digitalWrite(ain2_pin, LOW);
+    digitalWrite(bin1_pin, HIGH);
+    digitalWrite(bin2_pin, LOW);}
+   // analogWrite(pwma_pin, 50);
+ // analogWrite(pwmb_pin, 255);
+    Serial.println("right 90 degree ");
+    //delay(200);
+  //  brake();
+  r=0;
+  }
+   
+  }
+
+  // Brake
+  brake();
+}
+void drive(int power_a, int power_b) {
+
+  // Constrain power to between -255 and 255
+  power_a = constrain(power_a, 0, 100);
+  power_b = constrain(power_b, 0, 100);
+
+  // Left motor direction
+  if ( power_a < 0 ) {
+    digitalWrite(ain1_pin, LOW);
+    digitalWrite(ain2_pin, HIGH);
+  } else {
+    digitalWrite(ain1_pin, HIGH);
+    digitalWrite(ain2_pin, LOW);
+  }
+
+  // Right motor direction
+  if ( power_b < 0 ) {
+    digitalWrite(bin1_pin, LOW);
+    digitalWrite(bin2_pin, HIGH);
+  } else {
+    digitalWrite(bin1_pin, HIGH);
+    digitalWrite(bin2_pin, LOW);
+  }
+
+  // Set speed
+  analogWrite(pwma_pin, abs(power_a));
+  analogWrite(pwmb_pin, abs(power_b));
+}
+
+void brake() {
+  digitalWrite(ain1_pin, LOW);
+  digitalWrite(ain2_pin, LOW);
+  digitalWrite(bin1_pin, LOW);
+  digitalWrite(bin2_pin, LOW);
+  analogWrite(pwma_pin, 0);
+  analogWrite(pwmb_pin, 0);
+}*/
+
+void countLeft() {
+  enc_l++;
+}
+
+void countRight() {
+  enc_r++;
 }
