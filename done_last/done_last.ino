d12e2f05069c66403327b99e6f07960011028db5
Duha Jarrar <54679733+duhajarrar@users.noreply.github.com>
2021-02-08 16:15:13 +0200

    Update done_last.ino

--- done_last/done_last.ino
+++ done_last/done_last.ino
@@ -1,7 +1,4 @@
-// Include the TimerOne Library from Paul Stoffregen
-//#include "TimerOne.h"
-#include <NewPing.h>
-//#include <ArduinoRobot.h>
+
 // for 3 Ultra Sensor
 int trigPinL =A2;
 int echoPinL = A3;
@@ -11,9 +8,6 @@ int echoPinF = 13;
 
 int trigPinR = 5;
 int echoPinR = 4;
-
-
-
 int dir;
 
 
@@ -22,9 +16,6 @@ int dir;
 #define BACKWARD 2
 #define LEFT 3
 #define RIGHT 4
-
-
-
 #define MAX_DISTANCE 100 
 
 
@@ -33,16 +24,11 @@ boolean leftwall ;
 boolean rightwall ;
 
 int wall_threshold =12;
-
-//int left_threshold = 10 ;
-//int right_threshold = 10 ;
 int front_threshold = 12;
 
-// Constants for Interrupt Pins
-// Change values if not using Arduino Uno
 
-const byte MOTOR1 = 2;  // Motor 1 Interrupt Pin - INT 0
-const byte MOTOR2 = 3;  // Motor 2 Interrupt Pin - INT 1
+const byte MOTOR1 = A1;  // Motor 1 Interrupt Pin - INT 0
+const byte MOTOR2 = A0;  // Motor 2 Interrupt Pin - INT 1
 
 
  
@@ -51,20 +37,8 @@ int in1 = 6; //R
 int in2 = 7; 
 int in3 = 9; //L
 int in4 = 8;
-
 int ena = 10; //right
 int enb = 11; //left
-
-
-
-
-
-NewPing sonarLeft(trigPinL, echoPinL, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonarRight(trigPinR, echoPinR, MAX_DISTANCE);
-NewPing sonarFront(trigPinF, echoPinF, MAX_DISTANCE);
-
-
-
 
 float oldLeftSensor, oldRightSensor, leftSensor, rightSensor, frontSensor, oldFrontSensor, lSensor, rSensor, fSensor;
 
@@ -97,11 +71,6 @@ void setup() {
 }
 
 void loop() {
-
-//setDirection(LEFT);
-//setDirection(RIGHT);
-//setDirection(FORWARD);
-//setDirection(FORWARD);
 while(true){
 delay(500);  
 ReadSensors();
@@ -111,7 +80,7 @@ walls();
 if (frontwall == true ) {
     if (rightwall == true ){
       if (leftwall == true ){
-       setDirection(BACKWARD); 
+       setDirection(STOP); 
       }else{
         setDirection(LEFT);      
       }
@@ -124,74 +93,12 @@ if (frontwall == true ) {
 }else{
   setDirection(FORWARD);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
- /* 
- // if (!frontwall || !leftwall || !rightwall){
-ReadSensors();
-walls();
-/*Serial.println("frontwall");
-    Serial.println(frontwall);
-  Serial.println("leftwall");
-  Serial.println(leftwall);
-  Serial.println("rightwall");
-  Serial.println(rightwall);
-delay(500);
-if (frontwall == false ){
-  setDirection(FORWARD);
-}
-else if (leftwall == false ) {
- setDirection(LEFT);
-}else if(rightwall == false){
-  setDirection(RIGHT);
-}else{
- setDirection(STOP);
-}
-*/
-
-
 }
 }
 
 
 
 void ReadSensors() {
-
-//leftSensor = sonarLeft.ping_cm(); //ping in cm
-//rightSensor = sonarRight.ping_cm();
-//frontSensor = sonarFront.ping_cm();
-//Serial.print("left sensor : ");
-//Serial.println(leftSensor);
-//Serial.print("right sensor : ");
-//Serial.println(rightSensor);
-//Serial.print("front sensor : ");
-//Serial.println(frontSensor);
-//
-// oldLeftSensor = leftSensor; // save old readings for movment
-// oldRightSensor = rightSensor;
-// oldFrontSensor = frontSensor;
-
 long duration;
   digitalWrite(trigPinL, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -215,18 +122,12 @@ long duration;
   duration = pulseIn(echoPinR, HIGH);
   rightSensor = (duration/2) / 29.1; 
 
-
 Serial.print("left sensor : ");
 Serial.println(leftSensor);
 Serial.print("right sensor : ");
 Serial.println(rightSensor);
 Serial.print("front sensor : ");
 Serial.println(frontSensor);
-
-
-
-
-  
 }
 
 
@@ -258,17 +159,11 @@ void walls() {
 
 }
 
-
-
-
-
 void setDirection(int dir) {
  
   Serial.print("set Direction .. \n\n");
   if ( dir == FORWARD ) {
    Serial.print("GO FORWARD .. \n");
- // analogWrite(ena,110); 
-  //analogWrite(enb,130); 
   digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
@@ -278,20 +173,15 @@ void setDirection(int dir) {
   digitalWrite(in2, HIGH );
   digitalWrite(in3, HIGH );  // Right wheel forward
   digitalWrite(in4, HIGH);
-
-
   
   }
   else if ( dir == LEFT ) {
     Serial.print("GO LEFT .. \n");
-//
   digitalWrite(in1, LOW); 
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   delay(300);
-
-
     
   digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
@@ -302,10 +192,6 @@ void setDirection(int dir) {
   digitalWrite(in2, HIGH );
   digitalWrite(in3, HIGH );  // Right wheel forward
   digitalWrite(in4, HIGH);
-  
-
-
- // delay(100);
 
 
   
@@ -328,10 +214,6 @@ void setDirection(int dir) {
   digitalWrite(in2, HIGH );
   digitalWrite(in3, HIGH );  // Right wheel forward
   digitalWrite(in4, HIGH);
-
-
- // delay(100);
-
   
   }
   else if ( dir == STOP ) {
@@ -343,8 +225,6 @@ void setDirection(int dir) {
   }
   else if ( dir == BACKWARD ) {
   Serial.print("GO BACKWARD .. \n");
-  //  analogWrite(ena,110); 
-  //analogWrite(enb,130); 
   digitalWrite(in1, LOW); 
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
@@ -357,10 +237,5 @@ void setDirection(int dir) {
   digitalWrite(in3, HIGH );  // Right wheel forward
   digitalWrite(in4, HIGH);
 
-
-  
-  }
-
-
-  
+  }  
 }
